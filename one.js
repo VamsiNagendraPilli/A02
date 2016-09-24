335b4b71e3d920860c39f99f572eac3cc3497707
VamsiNagendraPilli <S528374@mail.nwmissouri.edu>
2016-09-23 23:27:00 -0500

    Add files via upload

---
+++ one.js
@@ -0,0 +1,9 @@
+function myFunction()
+{
+	//var de=document.getElementById("de").value;
+var de= $("#de").val();
+var ww=document.getElementById("ww").value;
+var result=ww*de;
+window.alert("the total cost is:" + result);
+};
+
